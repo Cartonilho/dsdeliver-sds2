ba48a59b02f6c9cc439864bac902e6ebd5f7ecd9
Felipe Cartonilho <felderfac@gmail.com>
2021-01-07 22:37:47 -0300

    Add formatPrice

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.css';
 import StepsHearder from './StepsHearder';
+import ProductList from './ProductsList';
+import { Product } from './types';
+import { fetchProducts } from '../api';
 
 
 function Orders(){
+    const [products,setProducts] = useState<Product[]>([]);
+    console.log(products);
+    useEffect(() =>{
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+    },[])
+
     return(
         <div className="orders-container">
             <StepsHearder />
+            <ProductList products={products} />
         </div>
     )
 }
