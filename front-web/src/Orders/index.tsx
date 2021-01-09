be8481d55fa77059259cfec2cf7a5912884acb67
Felipe Cartonilho <felderfac@gmail.com>
2021-01-08 22:54:20 -0300

    Pausa

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -5,10 +5,13 @@ import ProductList from './ProductsList';
 import { Product, OrderLocationdata } from './types';
 import { fetchProducts } from '../api';
 import OrderLocation from './OrderLocation';
+import OrderSummary from './OrderSummary';
+import Footer from '../Footer';
 
 
 function Orders(){
     const [products,setProducts] = useState<Product[]>([]);
+    const [selectedProducts,setSelectedProducts] = useState<Product[]>([]);
     const [orderLocation, setOrderLocation] =useState<OrderLocationdata>();
     console.log(products);
     useEffect(() =>{
@@ -17,12 +20,29 @@ function Orders(){
         .catch(error => console.log(error))
     },[])
 
+    const handleSelectProduct = (product: Product) => {
+        const isAlreadySelected = selectedProducts.some(item => item.id === product.id);
+      
+        if (isAlreadySelected) {
+          const selected = selectedProducts.filter(item => item.id !== product.id);
+          setSelectedProducts(selected);
+        } else {
+          setSelectedProducts(previous => [...previous, product]);
+        }
+      }
+
     return(
+       <>
         <div className="orders-container">
             <StepsHearder />
-            <ProductList products={products} />
+            <ProductList 
+                products={products}
+                onSelectProduct={handleSelectProduct} />
             <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
+            <OrderSummary />
         </div>
+        <Footer />
+       </>
     )
 }
 
